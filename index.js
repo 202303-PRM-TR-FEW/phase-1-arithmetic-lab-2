79c3bb1cfc286b826f157287f49826bc08fc47ba
Edriss572 <edriss_miakhel@yahoo.com>
2023-04-11 15:16:40 +0300

    The assigment is done

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 =2, num2 = 31, num3 = 6, num4 = 34;
 
+let  multiply = num1 * num2;
 // Write your code here
+
+let random = Math.floor(Math.random() * 10 ) + 1;
+
+let mod = num4 % num3;
+
+
+let max = 20;
